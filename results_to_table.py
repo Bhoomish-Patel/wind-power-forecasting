229470cf1b4f1804a3cd95d1d5c0cd5e70a78402
Bhoomish-Patel <bhoomishpatel05@gmail.com>
2025-10-09 16:42:51 +0530

    making results_to_table.p generic

--- results_to_table.py
+++ results_to_table.py
@@ -1,35 +1,36 @@
 import pandas as pd
 import os
-
+filetype = ['aci','withoutaci']
 models = ["ann", "lstm", "tcn"]
 losses = ["quantile", "tube"]
-summary_path = "results/withoutaci/withoutaci_summary_results.csv"
-os.makedirs(os.path.dirname(summary_path), exist_ok=True)
+for f in filetype:
+    summary_path = f"results/{f}/{f}_summary_results.csv"
+    os.makedirs(os.path.dirname(summary_path), exist_ok=True)
 
-summary_df = pd.DataFrame(columns=[
-    "model", "loss", "picp", "mpiw","standard_dev",
-    "time",
-])
-for model in models:
-    for loss_function in losses:
-        cur_filename = f"results/withoutaci/{model}_{loss_function}_results.csv"
-        if not os.path.exists(cur_filename):
-            print(f"File not found: {cur_filename}")
-            continue
-        df = pd.read_csv(cur_filename)
-        first_row = df.iloc[0]
-        PICP = first_row.get("picp", 0.0)
-        MPIW = first_row.get("mpiw", 0.0)
-        std_dev = first_row.get("standard_dev", 0.0) 
-        time_taken = first_row.get("time", 0.0)
-        entry = pd.DataFrame([{
-            "model": model,
-            "loss": loss_function,
-            "picp": PICP,
-            "mpiw": MPIW,
-            "standard_dev": std_dev,
-            "time": time_taken,
-        }])
-        summary_df = pd.concat([summary_df, entry], ignore_index=True)
-summary_df.to_csv(summary_path, index=False)
-print(f"WITHOUT ACI summary table saved to {summary_path}")
+    summary_df = pd.DataFrame(columns=[
+        "model", "loss", "picp", "mpiw","standard_dev",
+        "time",
+    ])
+    for model in models:
+        for loss_function in losses:
+            cur_filename = f"results/{f}/{model}_{loss_function}_results.csv"
+            if not os.path.exists(cur_filename):
+                print(f"File not found: {cur_filename}")
+                continue
+            df = pd.read_csv(cur_filename)
+            first_row = df.iloc[0]
+            PICP = first_row.get("picp", 0.0)
+            MPIW = first_row.get("mpiw", 0.0)
+            std_dev = first_row.get("standard_dev", 0.0) 
+            time_taken = first_row.get("time", 0.0)
+            entry = pd.DataFrame([{
+                "model": model,
+                "loss": loss_function,
+                "picp": PICP,
+                "mpiw": MPIW,
+                "standard_dev": std_dev,
+                "time": time_taken,
+            }])
+            summary_df = pd.concat([summary_df, entry], ignore_index=True)
+    summary_df.to_csv(summary_path, index=False)
+    print(f"{f} summary table saved to {summary_path}")
